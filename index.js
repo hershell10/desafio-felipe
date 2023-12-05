6fc429c826de5aa15ee86e56bc0e81fdd3e5fbe5
hershell10 <hershellsousa@gmail.com>
2023-12-04 22:51:45 -0300

    atualizei meu codigo, agora ele està funcinando, so falta um detalhe...

--- index.js
+++ index.js
@@ -6,34 +6,37 @@
 //Se XP for entre 8.001 e 9.000 = Ascendente
 //Se XP for entre 9.001 e 10.000= Imortal
 //Se XP for maior ou igual a 10.001 = Radiante
-let heroi =[nome = "Hercules",
-    xp = 7000]
-function levelHeroi(nivel){
-    console.log("O Herói de nome " +  heroi[0] + " está no nível de " + nivel)
-}
+let heroi =["Hercules", 7000]
+
+
+for (nivelHeroi in heroi){
+    console.log("O Herói de nome " +  heroi[0] + " está no nível de " + nivelHeroi)
+
+
 if (heroi[1] < 1000) {
-    return nivel = ferro;
+    return nivelHeroi = "ferro";
 
 }
 else if (heroi[1] === 1001 || 2000){
-    return nivel = brnze;
+    return nivelHeroi = "brnze";
 }
 else if (heroi[1] === 6001 || 7000){
-    return nivel = ouro;
+    return nivelHeroi = "ouro";
 
 }
 else if (heroi[1] === 7001 || 8000) {
-    return nivel = paltina;
+    return nivelHeroi = "paltina";
 
 }
 else if (heroi[1] === 8001 || 9000) {
-    return nivel = ascendente;
+    return nivelHeroi = "ascendente";
 
 }
 else if (heroi[1] === 9001 || 10000) {
-    return nivel = imortal;
+    return nivelHeroi = "imortal";
 }
 else if (heroi[1] >= 10001) {
-    return nivel = radiante;
+   return nivelHeroi = "radiante"
+}
 }
 
